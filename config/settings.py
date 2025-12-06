b3dae01387e7351225efd18cd1c0023df0a4a510
Bunyad Takhirov <takhirovbunyad@gmail.com>
2025-12-06 20:33:52 +0500

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-pf*2@ax0cfyc=2=077*xn_wwvop7c2f96d2_(^86kte%@yv3om'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
